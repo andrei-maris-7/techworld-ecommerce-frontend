8a3f64c3bbdb8fc8d1d694bd6750102bfc357d74
andrei.maris.7 <andreimaris90@gmail.com>
2021-11-10 23:24:38 +0200

    Changed font

--- pages/_document.js
+++ pages/_document.js
@@ -10,8 +10,12 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link
+          {/* <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          /> */}
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
         </Head>
