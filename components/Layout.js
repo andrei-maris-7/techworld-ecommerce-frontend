5a7e19181f571804873da7f53186875bca7b4efa
andrei.maris.7 <andreimaris90@gmail.com>
2021-11-10 23:41:58 +0200

    Added favicon again

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,7 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import Head from "next/head";
+
 import { useRouter } from "next/router";
 
 export default function Layout({ children }) {
@@ -8,6 +10,10 @@ export default function Layout({ children }) {
   return (
     <>
       <div className="min-h-screen relative flex flex-col">
+        <Head>
+          <link rel="shortcut icon" href="/techworld-single.png" />
+        </Head>
+
         <Header />
         <main className="mb-auto">{children}</main>
         {router.pathname !== "/login" && <Footer />}
